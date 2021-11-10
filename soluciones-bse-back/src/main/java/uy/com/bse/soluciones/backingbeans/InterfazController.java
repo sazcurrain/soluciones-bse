8a0053091aa644b7d6d554ae1fe82156155764c7
admin <admin@pcmariela>
2021-11-10 18:57:37 -0300

    ABM interfaz

---
+++ soluciones-bse-back/src/main/java/uy/com/bse/soluciones/backingbeans/InterfazController.java
@@ -0,0 +1,94 @@
+package uy.com.bse.soluciones.backingbeans;
+
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+import uy.com.bse.soluciones.domain.Interfaz;
+import uy.com.bse.soluciones.domain.Persona;
+import uy.com.bse.soluciones.domain.Profesion;
+import uy.com.bse.soluciones.domain.Enumeradores.Sexo;
+import uy.com.bse.soluciones.ejbs.InterfazService;
+import uy.com.bse.soluciones.ejbs.PersonaService;
+import uy.com.bse.soluciones.ejbs.ProfesionService;
+
+/**
+ * Backing Bean de las pantallas de Interfaz
+ * @author juan
+ *
+ */
+//TODO: Falta incorporar el manejo de excepciones y mostrarlas correctamente en JSF
+@Named("interfazController")
+@ViewScoped
+public class InterfazController implements Serializable {
+	
+
+	private static final long serialVersionUID = 1L;
+	
+	private Interfaz interfaz = new Interfaz();
+
+	
+	@EJB
+	InterfazService interfazService;
+	
+	
+	public InterfazController() {
+		
+	}
+
+	public Interfaz getInterfaz() {
+		return interfaz;
+	}
+
+	public void setInterfaz(Interfaz interfaz) {
+		this.interfaz = interfaz;
+	}
+	
+	/**
+	 * Retorno la lista de personas cargadas en la BD
+	 * @return List<Persona>
+	 */
+	public List<Interfaz> getListaInterfaces(){
+		return interfazService.getInterfaces();
+		
+	}
+	
+		/**
+	 * Agrega una nueva interfaz ala BD
+	 * @return String con la regla de navegacion
+	 */
+	public String crearInterfaz() {
+		interfazService.update(interfaz);					
+		return "interfaces.xhtml?faces-redirect=true";
+	}
+	
+	/**
+	 * Busca a una interfaz por el ID
+	 * Si la interfaz existe en la BD retorna esa interfaz
+	 * En caso contrario crea una nueva
+	 */
+	public void findInterfazById() {
+		if (interfaz.getId() != null) {
+			interfaz = interfazService.find(interfaz.getId());
+			if (interfaz == null) {
+				interfaz = new Interfaz();
+			}
+		}
+	}
+	
+	/**
+	 * Elimina una interfaz de la BD
+	 * @param p
+	 */
+	public void eliminar(Interfaz i) {
+		interfazService.delete(i);
+	}
+	
+	public boolean isManaged(Long id) {
+		return id != null;
+	}
+}
---
+++ soluciones-bse-ejb/src/main/java/uy/com/bse/soluciones/ejbs/InterfazService.java
@@ -0,0 +1,38 @@
+package uy.com.bse.soluciones.ejbs;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import uy.com.bse.soluciones.domain.Interfaz;
+import uy.com.bse.soluciones.domain.Stakeholder;
+
+/**
+ * Servicio EJB para la entity Interfaz
+ * @author juan
+ *
+ */
+@Stateless
+public class InterfazService extends AbstractService<Interfaz, Long>{
+	
+	@PersistenceContext(unitName = "soluciones_bse")
+	protected EntityManager em;
+		
+	
+	public InterfazService() {
+		super(Interfaz.class);
+	}	
+	
+	@SuppressWarnings("unchecked")
+	public List<Interfaz> getInterfaces() {
+		return em.createQuery("select i from Interfaz i order by i.nombre").getResultList();
+	}
+		
+	
+	@Override
+	public EntityManager getEntityManager() {
+		return em;
+	}
+}
