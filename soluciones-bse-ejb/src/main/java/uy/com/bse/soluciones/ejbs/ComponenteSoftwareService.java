0ec643f07d30ed4ad9d5577d568532ad20d57f87
sergio <sergio@sergio-GT680R>
2021-11-17 23:45:39 -0300

    Consulta avanzada

--- soluciones-bse-ejb/src/main/java/uy/com/bse/soluciones/ejbs/ComponenteSoftwareService.java
+++ soluciones-bse-ejb/src/main/java/uy/com/bse/soluciones/ejbs/ComponenteSoftwareService.java
@@ -2,10 +2,16 @@ package uy.com.bse.soluciones.ejbs;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Order;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import uy.com.bse.soluciones.domain.ComponenteSoftware;
 
@@ -23,6 +29,109 @@ public class ComponenteSoftwareService extends AbstractService<ComponenteSoftwar
 	public List<ComponenteSoftware> getComponentes(Class<? extends ComponenteSoftware> tipo) {
 		String sql = "select TREAT(cs as "+tipo.getName()+") from ComponenteSoftware cs order by cs.nombre";
 		return em.createQuery(sql).getResultList();
+	}
+	
+	private <T extends ComponenteSoftware>  Predicate predicateFromFilter(CriteriaBuilder cb, Root<T> root,
+			Map<String, String> filter) {
+		if (!filter.containsKey("field") || !filter.containsKey("value") || !filter.containsKey("op"))
+			return null;
+
+		Predicate p = null;
+		switch (filter.get("op")) {
+		case "=":
+			p = cb.equal(root.get(filter.get("field")), filter.get("value"));
+			break;
+		case ">":
+			p = cb.lessThan(root.get(filter.get("field")), filter.get("value"));
+			break;
+		case "<":
+			p = cb.greaterThan(root.get(filter.get("field")), filter.get("value"));
+			break;
+		case "contains":
+			p = cb.like(root.get(filter.get("field")), "%" + filter.get("value") + "%");
+			break;
+		case "starts":
+			p = cb.like(root.get(filter.get("field")), filter.get("value") + "%");
+			break;
+
+		default:
+			break;
+		}
+		return p;
+	}
+
+	private <T extends ComponenteSoftware>  Order predicateFromOrder(CriteriaBuilder cb, Root<T> root, Map<String, String> order) {
+		if (!order.containsKey("field") || !order.containsKey("order"))
+			return null;
+
+		Order p = null;
+		switch (order.get("order")) {
+		case "ASC":
+			p = cb.asc(root.get(order.get("field")));
+			break;
+		case "DESC":
+			p = cb.desc(root.get(order.get("field")));
+			break;
+		default:
+			break;
+		}
+		return p;
+	}
+	
+	public <T extends ComponenteSoftware> int countFilteredComponentes(Class<T> tipo, List<Map<String, String>> filters) {
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<Long> q = cb.createQuery(Long.class);
+		Root<T> root = q.from(tipo);
+		q.select(cb.count(root));
+		Predicate pFilters = null;
+		for (Map<String, String> filter : filters) {
+			if (pFilters != null) {
+				Predicate pFilter = predicateFromFilter(cb, root, filter);
+				if (pFilter != null)
+					pFilters = cb.and(pFilters, pFilter);
+			} else {
+				pFilters = predicateFromFilter(cb, root, filter);
+			}
+		}
+		if (pFilters != null)
+			q.where(pFilters);
+		
+
+		return em.createQuery(q).getSingleResult().intValue();
+	}
+
+	public <T extends ComponenteSoftware>  List<T> filterComponentes(Class<T> tipo, int offset, int count,
+			List<Map<String, String>> orders, List<Map<String, String>> filters) {
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<T> q = cb.createQuery(tipo);
+		Root<T> root = q.from(tipo);
+		Predicate pFilters = null;
+		for (Map<String, String> filter : filters) {
+			if (pFilters != null) {
+				Predicate pFilter = predicateFromFilter(cb, root, filter);
+				if (pFilter != null)
+					pFilters = cb.and(pFilters, pFilter);
+			} else {
+				pFilters = predicateFromFilter(cb, root, filter);
+			}
+		}
+		if (pFilters != null)
+			q.where(pFilters);
+
+		List<Order> pOrders = new ArrayList<Order>();
+		for (Map<String, String> order : orders) {
+			Order pOrder = predicateFromOrder(cb, root, order);
+			if (pOrder != null)
+				pOrders.add(pOrder);
+		}
+		
+		q.orderBy(pOrders);
+		
+
+		return em.createQuery(q)
+				.setFirstResult(offset)
+				.setMaxResults(count)
+				.getResultList();
 	}
 
 	@Override
